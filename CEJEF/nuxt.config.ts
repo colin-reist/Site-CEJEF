c3a01bb655ff726f23c2ea65835815d3f579c5ad
Colin Reist <94681686+colin-reist@users.noreply.github.com>
2025-11-13 10:04:53 +0100

    Correction for github-pages

--- CEJEF/nuxt.config.ts
+++ CEJEF/nuxt.config.ts
@@ -1,10 +1,12 @@
+const repoBase = '/Site-CEJEF/'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   ssr: false,
   app: {
-    baseURL: '/CEJEF/'
+    baseURL: import.meta.env.MODE === 'production' ? repoBase : '/'
   },
   nitro: {
     preset: 'github-pages'
